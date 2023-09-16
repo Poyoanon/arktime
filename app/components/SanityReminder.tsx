d24dc8160f3ec374d6d70038f80245dd308bb026
Poyoanon <27013504+Poyoanon@users.noreply.github.com>
2023-09-16 23:26:48 +0800

    Initial commit

---
+++ app/components/SanityReminder.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const MAX_SANITY = 135;
+
+const SanityReminder: React.FC = () => {
+  const [currentSanity, setCurrentSanity] = useState<number>(0);
+  const [timeToMaxSanity, setTimeToMaxSanity] = useState<string>("");
+  const [maxSanityTime, setMaxSanityTime] = useState<string>("");
+
+  useEffect(() => {
+    const calculateTimeToMaxSanity = () => {
+      const sanityDifference = MAX_SANITY - currentSanity;
+      const timeInMinutes = sanityDifference * 6;
+      const hours = Math.floor(timeInMinutes / 60);
+      const minutes = timeInMinutes % 60;
+
+      const currentTime = new Date();
+      currentTime.setMinutes(currentTime.getMinutes() + timeInMinutes);
+
+      const formatter = new Intl.DateTimeFormat("default", {
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      });
+
+      setTimeToMaxSanity(`${hours} hours and ${minutes} minutes`);
+      setMaxSanityTime(formatter.format(currentTime));
+    };
+
+    calculateTimeToMaxSanity();
+  }, [currentSanity]);
+
+  const handleSanityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newSanity = parseInt(event.target.value, 10);
+    newSanity = isNaN(newSanity)
+      ? 0
+      : newSanity < 0
+      ? 0
+      : newSanity > MAX_SANITY
+      ? MAX_SANITY
+      : newSanity;
+    setCurrentSanity(newSanity);
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <h1 className="text-center font-black">Arktime</h1>
+        <p>
+          A reminder app for letting you know when your Sanity will reach the
+          maximum in Arknights.
+        </p>
+        <label>
+          Current Sanity:
+          <input
+            className="text-black"
+            type="number"
+            value={currentSanity}
+            onChange={handleSanityChange}
+            min={0}
+            max={MAX_SANITY}
+          />
+        </label>
+        <p>Time to Max Sanity: {timeToMaxSanity}</p>
+        <p>Estimated Time: {maxSanityTime}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SanityReminder;
